ccbdd32e8a1a3e6ffaad228f12243576a00a2c61
gilcllys <gilcllyscosta@gmail.com>
2022-06-05 16:49:46 -0400

    Parei no creating the dating card components, continuar a partir desta parte. Problemas para resolver futuramente: as imagens não estão renderizando na tela, e não faço a mínima ideia porque, procurar solução depoiis !!!!!!!!!

--- dating-app-frontend/src/App.jsx
+++ dating-app-frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react"
+import DatingCards from "./components/DatingCard"
 import Header from "./components/Header"
 
 function App() {
-  const [count, setCount] = React.useState(0)
 
   return (
     <div className="App">
      <Header/> 
+     <DatingCards />
     </div>
   )
 }
